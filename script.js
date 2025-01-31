654d439f4da0892008bdbe20f786d51f882afa5d
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-01-31 23:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function stringChop(str, size) {
   // your code here
+	let ans=[];
+	for(let i=0;i<str.length;i+=size){
+		ans.push(str.slice(i,i+size));
+	}
+	return ans;
 }
 
 // Do not change the code below
